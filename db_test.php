fc98cb89f735b01f8540a04c3b33539085b795a5
Kent <kent24000@yahoo.co.jp>
2014-12-20 14:47:13 +0900

    Modified: mysql_connect() function -> mysqli_connect()

--- db_test.php
+++ db_test.php
@@ -1,7 +1,8 @@
-<?php> $link = mysql_connect('localhost', 'root', '');
+<?php
+ $link = mysqli_connect('localhost', 'root', '');
 if (!$link) {
 die('接続失敗！
-'.mysql_error());
+'.mysqli_connect_error());
 } else {
 echo '接続成功！';
 }
